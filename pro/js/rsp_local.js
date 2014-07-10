eeb4932b03500cdaf027168fc5934cb8baa08a80
proshunsuke <shunsuke0901@gmail.com>
2014-07-10 15:34:26 +0900

    #5 step2

--- pro/js/rsp_local.js
+++ pro/js/rsp_local.js
@@ -1,66 +1,98 @@
 $(function(){
-  "use strict";
-  var HAND_TYPE = {
-    ROCK : 0,
-    SCISSORS : 1,
-    PAPER : 2,
-  };
-  var RSP_RESULT_CODE = {
-    DRAW : 0,
-    WIN : 1,
-    LOSE : 2,
-  };
 
-  $(".rsp-btn").click(function(){
-    var result = judge(
-      myHand($(this).attr("id")),
-      bobHand()
-      );
-    showResult(result);
-  });
-  function myHand(handType) {
-    var hand;
-    if (handType == "rock") {
-      $("#myrspimg").attr("src", "img/rock.png");
-      hand = HAND_TYPE.ROCK;
-    } else if (handType == "scissors") {
-      $("#myrspimg").attr("src", "img/scissors.png");
-      hand = HAND_TYPE.SCISSORS;
-    } else {
-      $("#myrspimg").attr("src", "img/paper.png");
-      hand = HAND_TYPE.PAPER;
+    var winCount = 0;
+    var loseCount = 0;
+    var drawCount = 0;
+
+    "use strict";
+    var HAND_TYPE = {
+        ROCK : 0,
+        SCISSORS : 1,
+        PAPER : 2,
+    };
+    var RSP_RESULT_CODE = {
+        DRAW : 0,
+        WIN : 1,
+        LOSE : 2,
+    };
+
+    $(".rsp-btn").click(function(){
+        var bobhand = bobHand();
+        var result = judge(
+            myHand($(this).attr("id")),
+            bobhand
+        );
+
+        showResult(result,myHand($(this).attr("id")),bobhand);
+    });
+    function myHand(handType) {
+        var hand;
+        if (handType == "rock") {
+            $("#myrspimg").attr("src", "img/rock.png");
+            hand = HAND_TYPE.ROCK;
+        } else if (handType == "scissors") {
+            $("#myrspimg").attr("src", "img/scissors.png");
+            hand = HAND_TYPE.SCISSORS;
+        } else {
+            $("#myrspimg").attr("src", "img/paper.png");
+            hand = HAND_TYPE.PAPER;
+        }
+        return hand;
     }
-    return hand;
-  }
-  function bobHand() {
-    var hand = Math.floor(Math.random() * 3);
-    if (hand === HAND_TYPE.ROCK) {
-      $("#bobrspimg").attr("src", "img/rock.png");
-    } else if (hand === HAND_TYPE.SCISSORS) {
-      $("#bobrspimg").attr("src", "img/scissors.png");
-    } else {
-      $("#bobrspimg").attr("src", "img/paper.png");
+    function bobHand() {
+        var hand = Math.floor(Math.random() * 3);
+        if (hand === HAND_TYPE.ROCK) {
+            $("#bobrspimg").attr("src", "img/rock.png");
+        } else if (hand === HAND_TYPE.SCISSORS) {
+            $("#bobrspimg").attr("src", "img/scissors.png");
+        } else {
+            $("#bobrspimg").attr("src", "img/paper.png");
+        }
+        return hand;
     }
-    return hand;
-  }
-  function judge(myHand, otherHand) {
-    var result;
-    if (myHand === otherHand) {
-      result = RSP_RESULT_CODE.DRAW;
-    } else if ((myHand === HAND_TYPE.ROCK && otherHand === HAND_TYPE.SCISSORS) || (myHand === HAND_TYPE.SCISSORS && otherHand === HAND_TYPE.PAPER) || (myHand === HAND_TYPE.PAPER && otherHand === HAND_TYPE.ROCK)) {
-      result = RSP_RESULT_CODE.WIN;
-    }else {
-      result = RSP_RESULT_CODE.LOSE;
+    function judge(myHand, otherHand) {
+        var result;
+        console.log("myhand: " + myHand + ",otherHand: " + otherHand);
+        if (myHand === otherHand) { // draw
+            result = RSP_RESULT_CODE.DRAW;
+            drawCount++;
+        } else if ((myHand === HAND_TYPE.ROCK && otherHand === HAND_TYPE.SCISSORS) || (myHand === HAND_TYPE.SCISSORS && otherHand === HAND_TYPE.PAPER) || (myHand === HAND_TYPE.PAPER && otherHand === HAND_TYPE.ROCK)) {
+            result = RSP_RESULT_CODE.WIN;
+            winCount++;
+        }else {
+            result = RSP_RESULT_CODE.LOSE;
+            loseCount++;
+        }
+        return result;
     }
-    return result;
-  }
-  function showResult(result) {
-    if (result === RSP_RESULT_CODE.DRAW) {
-      $("#result").text("draw.");
-    } else if (result === RSP_RESULT_CODE.WIN) {
-      $("#result").text("You win!");
-    } else {
-      $("#result").text("You lose!");
+    function showResult(result,myhand,bobhand) {
+        var myhandshow = "";
+        var bobhandshow = "";
+        if(myhand == 0){ // ぐー
+            myhandshow = "グー";
+        }else if(myhand == 1){ // ちょき
+            myhandshow = "チョキ";
+        }else{ // パー
+            myhandshow = "パー";
+        }
+        if(bobhand == 0){ // ぐー
+            bobhandshow = "グー";
+        }else if(bobhand == 1){ // ちょき
+            bobhandshow = "チョキ";
+        }else{ // パー
+            bobhandshow = "パー";
+        }
+
+        if (result === RSP_RESULT_CODE.DRAW) {
+            $("#result").text("draw.");
+            $("#rireki").text("自分の手: " + myhandshow + ",相手の手: " + bobhandshow + "勝敗: 引き分け");
+        } else if (result === RSP_RESULT_CODE.WIN) {
+            $("#result").text("You win!");
+            $("#rireki").text("自分の手: " + myhandshow + ",相手の手: " + bobhandshow + "勝敗: 勝ち");
+        } else {
+            $("#result").text("You lose!");
+            $("#rireki").text("自分の手: " + myhandshow + ",相手の手: " + bobhandshow + "勝敗: 負け");
+        }
+        $("#summary").text(winCount + "勝" + loseCount + "負" + drawCount + "分け");
     }
-  }
 });
